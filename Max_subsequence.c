b560eea00e40f8c9612c1620bf8d4b3f87a97851
duhan666 <kiger-666@163.com>
2017-02-14 08:40:34 +0800

    201702140840

--- Max_subsequence.c
+++ Max_subsequence.c
@@ -2,6 +2,47 @@
 
 
 
+//solution 1:
+int Max_seq1(int a[],int N){
+    int maxsum = 0;
+    int sum=0;
+    int i,j;
+    for(i=0;i<N;i++){
+        for(j=i;j<N;j++){
+            sum += a[j];
+            //printf("%d\t",sum);
+            if(sum>maxsum)
+                maxsum = sum;
+        }
+        sum = 0;
+    }
+    return maxsum;
+}
+
+int Max_seq2(int a[],int N){
+    int maxsum = 0;
+    int sum = 0;
+    int i;
+
+    for(i=0;i<N;i++){
+        sum +=a[i];
+        if(sum > maxsum)        //ingenious method
+            maxsum = sum;
+        else if(sum < 0)
+            sum = 0;
+    }
+    return maxsum;
+}
+
+
+
+void main(){
+    int a[]  = {-2,11,-4,13,-5,-2};
+    int N =  sizeof(a)/sizeof(int);
+    printf("%d\n",Max_seq2(a,N));
+    printf("%d\n",Max_seq1(a,N));
+}
+
 
 
 
